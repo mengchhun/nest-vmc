603e19c02caa4f9ae62c81f5c2dfba11a9cf2572
Meng Chhun <mengchhunvun@gmail.com>
2022-11-10 11:50:56 +0700

    [Product Model] - Change SoldOut DataType

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -4,7 +4,7 @@ export const ProductSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
-    soldout: {type: Number, required: false}
+    soldout: {type: Boolean, required: false}
 });
 
 export interface Product extends mongoose.Document {
